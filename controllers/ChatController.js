a54be2cf9d8bdbac3ff4f123b730dd4751e9d770
Mr-Farooq-599 <127822114+Mr-Farooq-599@users.noreply.github.com>
2023-06-21 18:37:52 +0500

    issues fixed + get all chats API added
    issues fixed + get all chats API added

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -3,49 +3,71 @@ const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 
 const createChat = expressAsyncHandler(async (req, res) => {
-    const { userId } = req.body;
+  const { userId } = req.body;
+  console.log(req.body);
 
-    if (!userId) {
-        console.log("ID is required");
-        return res.sendStatus(401);
+  if (!userId) {
+    console.log("ID is required");
+    return res.sendStatus(401);
+  }
+
+  //Lets get data from chat table along with latest message
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  //now we need sender ID of latest message as well, lets do it
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  //lets check if found any data from DB & return. Else create the chat
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    var chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    //Lets query to db to create the new chat
+    try {
+      const createdChat = await Chat.create(chatData);
+      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).send(fullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
     }
-
-    //Lets get data from chat table along with latest message
-    var isChat = await Chat.find({
-        isGroupChat: false,
-        $and: [
-            { users: { $elemMatch: { $eq: user._id } } },
-            { users: { $elemMatch: { $eq: userId } } }
-        ]
-    }).populate("users", "-password").populate("latestMessage");
-
-    //now we need sender ID of latest message as well, lets do it
-    isChat = await User.populate(isChat, {
-        path: "latestMessage.sender",
-        select: "name pic email",
-    });
-
-    //lets check if found any data from DB & return. Else create the chat
-    if (isChat.length > 0) {
-        res.send(isChat[0]);
-    } else {
-        var chatData = {
-            chatName: "sender",
-            isGroupChat: false,
-            users: [req.user._id, userId]
-        }
-
-        //Lets query to db to create the new chat
-        try {
-            const createdChat = await Chat.creat(chatData);
-            const fullChat = await Chat.findOne({ _id: createChat._id }).populate("users", "-password");
-            res.status(200).send(fullChat);
-        } catch (error) {
-            res.status(400);
-            throw new Error(error.message);
-        }
-    }
-
+  }
 });
 
-module.exports = { createChat };
+const fetchChat = expressAsyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {}
+});
+
+module.exports = { createChat, fetchChat };
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { protect } = require("../middleware/AuthMiddleWare");
-const { createChat } = require("../controllers/ChatController");
+const { createChat, fetchChat } = require("../controllers/ChatController");
 const router = express.Router();
 
 router.route("/").post(protect, createChat);
-// router.route("/").get(protect, fetchChat);
+router.route("/").get(protect, fetchChat);
 // router.route("/group").post(protect, createGroupChat);
 // router.route("/rename-group").put(protect, renameGroupName);
 // router.route("/add-member").put(protect, addToGroup);
